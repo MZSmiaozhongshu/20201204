3c95c781c1dea0e843308908f3388e85364b7fbe
MZSmiaozhongshu <1203296386@qq.com>
2020-12-04 00:53:41 -0500

    new

--- 1.c
+++ 1.c
@@ -4,7 +4,7 @@ int main (int argc, const char *argv[])
 	printf("hello world!\n");
 	printf("hello world!\n");
 	printf("hello world!\n");
-         int a = 3;
+     int a = 3;
 	printf("123");
 	return 0;
 }
